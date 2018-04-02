a27ec5c1056ed8ee4a73ed37c0c2afcdb3a03cbc
ewalu <ewa_lugin@tlen.pl>
2018-04-02 17:09:35 +0200

    udało się

--- EPO/WebContent/app.js
+++ EPO/WebContent/app.js
@@ -3,10 +3,14 @@ const app = angular.module("app", ['ui.router']);
 app.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider
-        .when('/', '/polisa')
-        .otherwise('/polisa');
+        .when('/', '/menu')
+        .otherwise('/menu');
 
     $stateProvider
+        .state("menu", {
+            url: '/menu',
+            templateUrl: "menu.html"
+        })
         .state("polisa", {
             url: '/polisa',
             templateUrl: "rejestracjapolisy.html"
