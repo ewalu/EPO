b753bf3ea09d8aa1c7a124a1217421f5680e77f9
ewalu <ewa_lugin@tlen.pl>
2018-08-16 14:29:06 +0200

    20180816 validatePolicy

--- EPO/src/pl/edu/atena/biz/polisa/PolicyValidateMethod.java
+++ EPO/src/pl/edu/atena/biz/polisa/PolicyValidateMethod.java
@@ -3,6 +3,7 @@ package pl.edu.atena.biz.polisa;
 import pl.edu.atena.entities.Message;
 import pl.edu.atena.entities.Policy;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 public class PolicyValidateMethod {
@@ -13,9 +14,25 @@ public class PolicyValidateMethod {
 			if(policy.getPolicyNumber().isEmpty()) {
 				policy.addMessageToPolicy(Message.POLICY_NUMBER_NO_DATA);
 			}
+			this.validatePremium(policy);
+			this.validatePeriod(policy);
 			
 		}
 		
+	}
+	
+	private void validatePremium(Policy policy) {
+		if(policy.getPremium().equals(null) || policy.getPremium().compareTo(BigDecimal.ZERO)<0) {
+			policy.addMessageToPolicy(Message.NEGATIVE_PREMIUM);
+		}
+	}
+	
+	private void validatePeriod(Policy policy) {
+		if(policy.getInsuranceEndDate().equals(null) || policy.getInsuranceStartDate().equals(null)) {
+			policy.addMessageToPolicy(Message.PERIOD_DATE_NO_DATA);
+		} else if(policy.getInsuranceEndDate().compareTo(policy.getInsuranceStartDate())<0) {
+			policy.addMessageToPolicy(Message.INCORECT_PERIOD);
+		}
 	}
 
 }
--- EPO/src/pl/edu/atena/entities/Message.java
+++ EPO/src/pl/edu/atena/entities/Message.java
@@ -3,7 +3,8 @@ package pl.edu.atena.entities;
 public enum Message {
 	POLICY_NUMBER_NO_DATA(0, "Brak numeru polisy"),
 	NEGATIVE_PREMIUM(1,"Niepoprawna skladka"),
-	INCORECT_PERIOD(2, "Niepoprawny okres ochorony");
+	INCORECT_PERIOD(2, "Niepoprawny okres ochorony"),
+	PERIOD_DATE_NO_DATA(3, "Brak jedenej z dat okresu ochrony");
 	
 	int code;
 	String text;
